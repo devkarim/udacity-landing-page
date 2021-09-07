aef3b9982846cc7c352ef6543230c6794805b577
devkarim <devkarim@hotmail.com>
2021-09-07 21:40:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Simple Udacity Landing Page Project
+
+## Description
+
+A simple landing page with multiple sections for a Udacity course.
+
+## Features
+
+- Navbar with smooth scroll.
+- Sections are highlighted when you scroll.
+- Dynamically add navbar items depending on sections.
+- Ability to collapse sections.
+- Scroll up when you are at the bottom of the page.
+- Auto-hide navbar when user stopped scrolling.
+
+## Dependencies
+
+- Bootstrap @5.1.1
+- Fontawesome @5.10
---
+++ js/app.js
@@ -0,0 +1,166 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+const allSections = document.getElementsByTagName('section');
+const navList = document.getElementById('navbar__list');
+const scrollToUp = document.getElementById('scrollTop');
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+/**
+ * @description Checks whether an element is in viewport
+ * @param {HTMLElement} element Element to check if in viewport
+ * @returns {boolean} If element is in viewport
+ */
+const isInViewport = (element) => {
+  const { top, bottom } = element.getBoundingClientRect();
+  return top >= 0 && bottom <= window.innerHeight;
+};
+
+/**
+ * @description Get all sections data in a good format
+ * @returns {Array} Array of sections data
+ */
+const getAllSectionsData = () => {
+  const sections = [];
+  for (const section of allSections) {
+    sections.push({
+      id: section.id,
+      text: section.getAttribute('data-nav'),
+    });
+  }
+  return sections;
+};
+
+const ALL_SECTIONS_DATA = getAllSectionsData();
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+/**
+ * @description Adds navbar items to navbar list
+ */
+const buildNavBar = () => {
+  const fragment = document.createDocumentFragment();
+  for (const section of ALL_SECTIONS_DATA) {
+    const li = document.createElement('li');
+    const link = document.createElement('a');
+    link.id = `${section.id}-item`;
+    link.setAttribute('href', '#' + section.id);
+    link.textContent = section.text;
+    link.className = 'menu__link';
+    li.appendChild(link);
+    fragment.appendChild(li);
+  }
+  navList.appendChild(fragment);
+};
+
+buildNavBar();
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+let scrollTimer;
+
+/**
+ * @description Hide/show navbar
+ * @param {boolean} show Whether to show or hide
+ */
+const setNavbarVisible = (show) => {
+  navList.style = `display: ${show ? 'block' : 'none'};`;
+};
+
+/**
+ * @description Hide/show navbar
+ * @param {boolean} show Whether to show or hide
+ */
+const setScrollToUpVisislbe = (show) => {
+  scrollToUp.style = `display: ${show ? 'block' : 'none'};`;
+};
+
+/**
+ * @description Multiple checks to user scroll like if user stopped scrolling to hide navbar, etc
+ * @param {Event} e Whether to show or hide
+ */
+const checkUserScroll = (e) => {
+  // Show scroll to up button if user is at the bottom of the page
+  const isAtTheBottom =
+    window.innerHeight + window.scrollY >= document.body.offsetHeight;
+  if (isAtTheBottom) {
+    setScrollToUpVisislbe(true);
+  } else {
+    setScrollToUpVisislbe(false);
+  }
+  // Stop scroll if user stopped scrolling for a while
+  setNavbarVisible(true);
+  if (scrollTimer) clearTimeout(scrollTimer);
+  scrollTimer = setTimeout(() => setNavbarVisible(false), 5000);
+};
+
+document.addEventListener('scroll', checkUserScroll);
+
+/**
+ * @description Checks active status of navbar items
+ */
+const checkActiveSection = (_) => {
+  for (const section of allSections) {
+    if (isInViewport(section)) {
+      const activeSection = document.querySelector('section.active');
+      const link = document.querySelector('#' + section.id + '-item');
+      const activeLink = document.querySelector('a.active');
+      if (activeSection != section) {
+        activeSection?.classList?.remove('active');
+        activeLink?.classList?.remove('active');
+        section.classList.add('active');
+        link.classList.add('active');
+      }
+      return;
+    }
+  }
+};
+document.addEventListener('scroll', checkActiveSection);
+
+// Check active section on start
+checkActiveSection();
+
+/**
+ * @description Scroll to section once user clicks
+ */
+navList.addEventListener('click', (e) => {
+  if (e.target.tagName != 'A') return;
+  e.preventDefault();
+  const element = document.querySelector(e.target.getAttribute('href'));
+  element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+});
+
+/**
+ * @description Scroll to up once user clicks
+ */
+scrollToUp.addEventListener('click', (_) => {
+  window.scrollTo(0, 0);
+});
